4261230d3d1b079b3cb660ebc35f4ba6a2659679
ashwinadiga01 <ashwinadiga0111@gmail.com>
2022-05-22 00:54:09 +0530

    Your extensions added in home  screen ✅

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, HStack, Text, VStack } from "@chakra-ui/react";
+import { Container, HStack, Text, VStack, Image } from "@chakra-ui/react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import GraphContainer from "../components/GraphContainer";
@@ -7,11 +7,29 @@ import BlurredBox from "../components/BlurredBox";
 import WelcomeBox from "../components/WelcomeBox";
 import DashboardNav from "../components/DashboardNav";
 import { getUserInfo } from "../helpers/getUserInfo";
+import notifications_btn from "../assets/notification_btn.png";
+import todo_btn from "../assets/todo_btn.png";
+import notes_btn from "../assets/notes_btn.png";
+import blurred_box_bg from "../assets/blurred_box_bg2.png";
 
 const HomeScreen = () => {
   const [date, setDate] = useState(new Date());
   const [userDetails, setUserDetails] = useState({});
-  const apps = [1, 2, 3];
+  const apps = [1];
+  const extensions = [
+    {
+      name: "Notifications",
+      image: notifications_btn,
+    },
+    {
+      name: "Todos",
+      image: todo_btn,
+    },
+    {
+      name: "Notes",
+      image: notes_btn,
+    },
+  ];
 
   useEffect(() => {
     const getDetails = async () => {
@@ -37,14 +55,45 @@ const HomeScreen = () => {
         <WelcomeBox name={userDetails.name} />
         <Calendar onChange={setDate} value={date} className="calendar" />
       </HStack>
-      <HStack pt={6} spacing={6}>
+      <HStack pt={4} spacing={6}>
         <GraphContainer />
         <VStack>
-          <Text fontWeight="bold" fontSize="xl" color="white" pb={6}>
+          <Text fontWeight="bold" fontSize="md" color="white" pb={1}>
             Top extensions
           </Text>
           {apps.map((e) => (
             <BlurredBox key={e} />
+          ))}
+
+          <Text fontWeight="bold" color="white" pt={1}>
+            Your extensions
+          </Text>
+          {extensions.map((t) => (
+            <Container
+              width={48}
+              height={14}
+              bgImage={blurred_box_bg}
+              bgPosition="center"
+              centerContent
+              pt={2}
+              borderRadius="lg"
+              cursor="pointer"
+            >
+              <Container
+                className="blur"
+                width={44}
+                height={10}
+                centerContent
+                borderRadius="lg"
+              >
+                <HStack pt={1} gridGap={1}>
+                  <Image src={t.image} width={8} />
+                  <Text color="white" fontSize="md">
+                    {t.name}
+                  </Text>
+                </HStack>
+              </Container>
+            </Container>
           ))}
         </VStack>
       </HStack>
