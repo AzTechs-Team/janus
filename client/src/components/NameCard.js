5456a8b7629c176325673f893458c4201187e2c3
ashwinadiga01 <ashwinadiga0111@gmail.com>
2022-05-22 12:45:15 +0530

    Updated notification overview/about in Extension screen ✅

--- client/src/components/NameCard.js
+++ client/src/components/NameCard.js
@@ -31,7 +31,12 @@ const NameCard = ({ content, id }) => {
         alt="Dan Abramov"
       />
       <VStack alignItems="flex-start" color="white" width="45%">
-        <Text fontSize="3xl" fontWeight="extrabold" letterSpacing={1}>
+        <Text
+          fontSize="3xl"
+          fontWeight="extrabold"
+          width="40vw"
+          letterSpacing={1}
+        >
           {content["title"]}
         </Text>
         <Text fontSize="sm">{content["description"]}</Text>
